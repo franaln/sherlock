cbd36c29e484f6bd4e89e1ccac9b14babbb671a6
franaln <franaln@gmai.com>
2014-09-15 00:53:39 -0300

    add function to compute the Levenshtein distance between strings

--- utils.py
+++ utils.py
@@ -103,6 +103,27 @@ INITIALS = string.ascii_uppercase + string.digits
 # Split on non-letters, numbers
 split_on_delimiters = re.compile('[^a-zA-Z0-9]').split
 
+
+def distance(str1, str2):
+    """ return the Levenshtein distance
+    between two strings """
+
+    d = dict()
+    for i in range(len(str1)+1):
+        d[i] = dict()
+        d[i][0] = i
+
+    for i in range(len(str2)+1):
+        d[0][i] = i
+
+    for i in range(1, len(str1)+1):
+        for j in range(1, len(str2)+1):
+            d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1,
+                          d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
+
+    return d[len(str1)][len(str2)]
+
+
 def filter(query, items, key=lambda x: x, ascending=False,
            include_score=True, min_score=0, max_results=0,
            match_on=MATCH_ALL ^ MATCH_ALLCHARS):
