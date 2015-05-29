4d2e94e18a97b2c41b852dff1d33b6a69329faf9
franaln <franaln@gmai.com>
2015-05-29 00:42:46 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sherlock',
+    version='0.1',
+    description='',
+    url='https://github.com/franaln/sherlock',
+    author='Francisco Alonso',
+    author_email='franaln@gmail.com',
+    license='',
+    packages=['sherlock'],
+    scripts=['sherlock.py',],
+)
