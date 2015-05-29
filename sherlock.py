4f500e662a3b41149b3710b9d7de6b4edd0cf7e7
franaln <franaln@gmai.com>
2015-05-29 17:39:34 -0300

    fix package to work installed in /usr

--- sherlock.py
+++ sherlock.py
@@ -1,7 +1,12 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 # Sherlock
 
+import os
 import sys
+
+# Don't import ./sherlock.py when running an installed binary at /usr/.../sherlock.py
+if __file__[:4] == '/usr' :
+    sys.path.remove(os.path.dirname(__file__))
 
 from sherlock.main import Sherlock
 
